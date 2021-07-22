91344575ce8316d5f9ce2aec4a9a780d79648671
hoodbu <38543715+hoodbu@users.noreply.github.com>
2021-07-22 19:05:58 -0400

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.0.2"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    workspaces {
+      name = "ace-iac-day-two"
+    }
+  }
+}
