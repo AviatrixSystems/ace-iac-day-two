3ffcd26b26f2013f535019351205947a9c1735c5
hoodbu <38543715+hoodbu@users.noreply.github.com>
2021-07-22 15:38:15 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,3 @@
 provider "aviatrix" {
   controller_ip = var.controller_ip
-  username      = var.username
-  password      = var.password
-}
-
-provider "aws" {
-  alias  = "canada"
-  region = "ca-central-1"
 }
