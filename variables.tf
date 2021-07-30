b353aa390fe3f00cf2d7490fa213681083980429
hoodbu <38543715+hoodbu@users.noreply.github.com>
2021-07-29 22:39:59 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "controller_ip" {
   type = string
 }
-variable "aws_spoke2_name" {
-  default = "ace-aws-ca-central-1-spoke2"
+variable "azure_spoke2_name" {
+  default = "ace-iac-spoke2"
 }
