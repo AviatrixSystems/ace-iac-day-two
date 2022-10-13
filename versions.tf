6dd1531b212dc69d8b44026ac42cf9f7ff858049
karimjamali <karim.jamali@gmail.com>
2022-10-13 14:39:37 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 2.22.1"
+      version = "2.24.0"
     }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
     }
   }
   required_version = ">= 1.0"
