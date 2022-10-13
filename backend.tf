2adb1c49a713ae8e7357d3c0f13b1712c83549dc
karimjamali <karim.jamali@gmail.com>
2022-10-13 14:41:02 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "ace-iac-day-two"
     workspaces {
       name = "ace-iac-day-two"
     }
