35c58b58efb05a7a1b0792f1e5ce9475cabd94d7
swelshavx <115665878+swelshavx@users.noreply.github.com>
2022-10-18 15:36:24 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,17 @@
+#terraform {
+ # required_version = ">= 1.1.0"
+#  backend "remote" {
+   #   organization = "Aviatrix-swelsh"
+   # workspaces {
+   #   name = "ace-iac-day-two"
+   # }
+#  }
+#}
+
 terraform {
-  required_version = ">= 1.1.0"
-  backend "remote" {
-      organization = "Aviatrix-swelsh"
+  cloud {
+    organization = "Aviatrix-swelsh"
+
     workspaces {
       name = "ace-iac-day-two"
     }
