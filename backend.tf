265adf37c7bd4bc5ac60e6606e70ac5f8aa755db
NetOPsMCSTech <114741130+NetOPsMCSTech@users.noreply.github.com>
2022-10-20 08:59:09 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "MCS_NetOPs"
+    organization = "MCS_NetOPs"
     workspaces {
       name = "ace-iac-day-two"
     }
