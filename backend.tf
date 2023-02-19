5dfaecb820c5031ec0efc334bd6eef55ac3f3547
NetOpsllaw <125761155+NetOpsllaw@users.noreply.github.com>
2023-02-19 11:16:04 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
+    hostname = "app.terraform.io"
     organization = "netops2023"
     workspaces {
       name = "ace-iac-day-two"
