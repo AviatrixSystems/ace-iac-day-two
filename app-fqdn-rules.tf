3da0dbb49d2c77d8bd79fbb0005762385d63f73d
Jemuel B Jeroham <130841268+jemuelbjeroham@users.noreply.github.com>
2023-04-16 01:15:21 +0530

    Adding *.google.com
    Adding *.google.com

--- app-fqdn-rules.tf
+++ app-fqdn-rules.tf
@@ -5,7 +5,8 @@ locals {
       "*.aviatrix.com" = "443"
       "aviatrix.com"   = "80"
       "*.ubuntu.com"   = "80"
-      "google.com"     = "443"   
+      "google.com"     = "443"
+      "*.google.com"     = "443"  
 
     }
     udp = {
