f40cfdae15b81508a82d20a4f0eb3bd551c7260c
JubeJube22 <133391043+JubeJube22@users.noreply.github.com>
2023-05-30 21:15:19 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    organization = "<samsheavydreams>"
+    organization = "samsheavydreams"
     workspaces {
       name = "ace-iac-day-two"
     }
