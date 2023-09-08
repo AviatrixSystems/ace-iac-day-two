9bb7f9f8161dc11d683afc24216363f9c70bd334
Nidip12 <143867732+Nidip12@users.noreply.github.com>
2023-09-09 04:43:41 +0530

    Update app-fqdn-rules.tf

--- app-fqdn-rules.tf
+++ app-fqdn-rules.tf
@@ -5,6 +5,7 @@ locals {
       "*.aviatrix.com" = "443"
       "aviatrix.com"   = "80"
       "*.ubuntu.com"   = "80"
+      "*.ubuntu.com"   = "443"
     }
     udp = {
       "dns.google.com" = "53"
