16dcc2e63502186c9c7c9ea4851a3f740f705a01
root <root@ip-172-31-45-151.ec2.internal>
2023-10-05 07:24:28 +0000

    day 2

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "jshome568"
     workspaces {
       name = "ace-iac-day-two"
     }
